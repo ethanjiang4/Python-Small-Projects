9a60386d7267ea7e8871ae52a8d1f2700604e0c9
Ethan Jiang <yyjsideshow@gmail.com>
2018-07-20 15:44:32 -0400

    Add files via upload

---
+++ main[1].py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import json
+import turtle
+import urllib.request
+
+url='http://api.open-notify.org/astros.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+print('People in Space: ', result['number'])
+
+people = result['people']
+
+for p in people:
+  print(p['name'] + ' in ' + p['craft'])
+  
+url='http://api.open-notify.org/iss-now.json'
+response = urllib.request.urlopen(url)
+result = json.loads(response.read())
+
+location = result['iss_position']
+lat = location['latitude']
+lon = location['longitude']
+print('Latitude: ', lat)
+print('Longitude: ', lon)
+
+screen=turtle.Screen()
+screen.setup(720, 360)
+screen.setworldcoordinates(-180, -90, 180, 90)
+screen.bgpic('map.jpg')
+
+screen.register_shape('iss.png')
+iss = turtle.Turtle()
+iss.shape('iss.png')
+iss.setheading(90)
+
+iss.penup()
+iss.goto(lon, lat)
